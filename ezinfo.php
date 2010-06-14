500828359248a19b4919147fc7346fa7c22e0767
Olivier Portier <op@ez.no>
2010-06-14 10:16:01 +0000

    Update version number to 0.2

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class ezworkflowcollectionInfo
     static function info()
     {
         return array( 'Name' => "ezworkflowcollection",
-                      'Version' => "0.1",
+                      'Version' => "0.2",
                       'Copyright' => "Copyright (C) 20010 G. Giunta - O. portier",
                       'License' => "GNU General Public License v2.0"
                      );
