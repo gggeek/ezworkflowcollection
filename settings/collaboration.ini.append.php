f187276c96a98fad959081b79be5323c575066c1
Olivier Portier <op@ez.no>
2010-06-14 10:07:45 +0000

    Approve location event type settings

---
+++ settings/collaboration.ini.append.php
@@ -0,0 +1,9 @@
+<?php /* #?ini charset="utf8"?
+
+[HandlerSettings]
+Extensions[]=ezworkflowcollection
+Repositories[]=extension/ezworkflowcollection/collaborationhandlers
+Active[]=approvelocation
+
+
+*/ ?>
--- settings/workflow.ini.append.php
+++ settings/workflow.ini.append.php
@@ -1,14 +1,21 @@
 <?php /*
 
+
+[OperationSettings]
+AvailableOperationList[]=content_addlocation
+
 [EventSettings]
 ExtensionDirectories[]=ezworkflowcollection
 
+AvailableEventTypes[]=event_approvelocation
 AvailableEventTypes[]=event_multipublish
 AvailableEventTypes[]=event_subtreemultiplexer
+
 # the following one needs classes found in ezflow
 AvailableEventTypes[]=event_expireremotecacheflow
 
 #AvailableEventTypes[]=event_relatetonode
 #AvailableEventTypes[]=event_relatetonodeattribute
+
 
 */ ?>
