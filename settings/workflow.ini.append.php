f612407708622b5072db06332b5d8bbd0f1c66b2
Olivier Portier <op@ez.no>
2010-12-08 15:40:21 +0000

    Add Object State update workflow event in workflow.ini file.

--- settings/workflow.ini.append.php
+++ settings/workflow.ini.append.php
@@ -11,11 +11,16 @@ AvailableEventTypes[]=event_approvelocation
 AvailableEventTypes[]=event_multipublish
 AvailableEventTypes[]=event_subtreemultiplexer
 
+# This one needs object states
+AvailableEventTypes[]=event_objectstateupdate
+
 # the following one needs classes found in ezflow
 AvailableEventTypes[]=event_expireremotecacheflow
 
 #AvailableEventTypes[]=event_relatetonode
 #AvailableEventTypes[]=event_relatetonodeattribute
+
+
 
 
 */ ?>
