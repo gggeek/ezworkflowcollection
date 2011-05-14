9402a7537457f593fe9f35788ccc6b646848b8af
gggeek <gg@ez.no>
2011-05-14 13:09:20 +0000

    - fix oracle schema script

--- sql/oracle/schema.sql
+++ sql/oracle/schema.sql
@@ -1,13 +1,13 @@
 
 CREATE TABLE ezxapprovelocation_items (
   collaboration_id INTEGER DEFAULT 0 NOT NULL,
-  id int(11) NOT NULL,
+  id INTEGER NOT NULL,
   workflow_process_id INTEGER DEFAULT 0 NOT NULL,
-  target_node_ids VARCHAR2(255) NOT NULL default '', -- !!
+  target_node_ids VARCHAR2(255) default '' NOT NULL, -- !!
   PRIMARY KEY  ( id )
 );
 
-CREATE SEQUENCE s_approvelocation_items
+CREATE SEQUENCE s_approvelocation_items;
 
 CREATE TRIGGER ezxapprovelocation_items_id_tr
 BEFORE INSERT ON ezxapprovelocation_items FOR EACH ROW WHEN (new.id IS NULL)
