4208979bbced921349182fa18fdc3a54ce604a08
gggeek <gg@ez.no>
2013-07-15 17:12:19 +0200

    update docs

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class ezworkflowcollectionInfo
     static function info()
     {
         return array( 'Name' => "ezworkflowcollection",
-                      'Version' => "0.5-dev",
+                      'Version' => "0.6-dev",
                       'Copyright' => "Copyright (C) 2010-2012 G. Giunta - O. Portier",
                       'License' => "GNU General Public License v2.0"
                      );
