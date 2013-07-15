1a1603ae4d1d42a47f350f0c356908a23bdbfc7f
gggeek <gg@ez.no>
2013-07-15 17:09:48 +0200

    update cronjobs: remove inclusion of nonexisting file; remove copied-over copyright text

--- cronjobs/publish.php
+++ cronjobs/publish.php
@@ -1,35 +1,6 @@
 <?php
-//
-// Created on: <09-12-2010 17:08:52 op>
-//
-// SOFTWARE NAME: eZ Publish
-// SOFTWARE RELEASE: 4.2.0
-// BUILD VERSION: 24182
-// AUTHOR : Olivier PORTIER eZ SYSTEMS WE
-// COPYRIGHT NOTICE: Copyright (C) 1999-2009 eZ Systems AS
-// SOFTWARE LICENSE: GNU General Public License v2.0
-// NOTICE: >
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of version 2.0  of the GNU General
-//   Public License as published by the Free Software Foundation.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of version 2.0 of the GNU General
-//   Public License along with this program; if not, write to the Free
-//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//   MA 02110-1301, USA.
-//
-//
-
-/*! \file
-*/
 
 // Check for extension
-require_once( 'kernel/common/ezincludefunctions.php' );
 eZExtension::activateExtensions();
 // Extension check end
 
