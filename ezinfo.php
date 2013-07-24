7f4a125c863461990d3390b2968cb25fa5e9518f
gggeek <gg@ez.no>
2013-07-24 12:14:37 +0200

    tag for post-release 0.6

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class ezworkflowcollectionInfo
     static function info()
     {
         return array( 'Name' => "ezworkflowcollection",
-                      'Version' => "0.6-dev",
+                      'Version' => "0.7-dev",
                       'Copyright' => "Copyright (C) 2010-2012 G. Giunta - O. Portier",
                       'License' => "GNU General Public License v2.0"
                      );
